0bd80c29b59acc608f4c4a16143120535c847f6f
Thoro87 <thorondors@gmx.de>
2018-02-13 18:48:24 +0100

    Switched to Razor View
    summary is now completely in razor

--- CoinView/Controllers/SummaryController.cs
+++ CoinView/Controllers/SummaryController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using CoinView.Models.Database;
+using CoinView.Models;
 
 namespace CoinView.Controllers {
     public class SummaryController : Controller {
@@ -14,24 +15,32 @@ namespace CoinView.Controllers {
             db = _context;
         }
 
-        public JsonResult GetData() {
-            Dictionary<string, object> data = new Dictionary<string, object>();
-
-            data["CoinValues"] = db.CoinValues.GroupBy(c => c.CoinId).ToDictionary(g => g.Key, g => g.OrderByDescending(c => c.Date).First());
-            data["Buys"] = db.Buys.Where(b => b.UserId == 1).Select(b => new { b.BuyId, b.UserId, b.Date, b.Purpose, b.ExchangeWalletId, b.WalletId, b.CoinId, b.PriceEur, b.AmountBought, b.AmountInWallet }).ToList();
-            data["OpenTrades"] = db.Trades.Where(t => t.UserId == 1 && t.SellWallet == null).Select(t => new { t.TradeId, t.UserId, t.StoreWalletId, t.CoinId, t.Amount, t.BuyWalletId, t.BuyPricePerShare, t.BuyPriceBtc, t.BuyDate, t.SellWalletId, t.SellPricePerShare, t.SellPriceBtc, t.SellDate }).ToList();
-            data["OpenCreations"] = db.Creations.Where(c => c.UserId == 1 && c.SellWallet == null).Select(c => new { c.CreationId, c.UserId, c.WalletId, c.CoinId, c.Amount, c.SellWalletId, c.SellDate, c.SellPricePerShare, c.SellPriceBtc }).ToList();
-
-
-            //data["Users"] = db.Users.Select(u => new { u.UserId, u.Name }).ToList();
-            //data["Coins"] = db.Coins.Select(c => new { c.CoinId, c.CoinMarketCapId, c.Name, c.Symbol }).ToDictionary(c => c.CoinId);
-            //data["Wallets"] = db.Wallets.Select(w => new { w.WalletId, w.Name }).ToDictionary(w => w.WalletId);
-
-            return Json(data);
-        }
-
         public IActionResult Summary() {
-            return View();
+
+            UserSummaryDO userSummary = GetSummaryForUser(1);
+
+            return View(userSummary);
         }
+
+        private UserSummaryDO GetSummaryForUser(int userID) {
+
+            List<Buy> buys = db.Buys.Where(b => b.UserId == userID).ToList();
+            Dictionary<int, CoinValue> coinValues = db.CoinValues.GroupBy(c => c.CoinId).ToDictionary(g => g.Key, g => g.OrderByDescending(c => c.Date).First());
+            List<Trade> trades = db.Trades.Where(t => t.UserId == userID && t.SellWallet == null).ToList();
+            List<Creation> creations = db.Creations.Where(c => c.UserId == userID && c.SellWallet == null).ToList();
+
+            return new UserSummaryDO() {
+                UserID = userID,
+                InvestsBuyValueEUR = buys.Where(b => b.Purpose == "Invest").Select(b => b.AmountBought * b.PriceEur).Sum(),
+                InvestsSellValueEUR = buys.Where(b => b.Purpose == "Invest").Select(b => b.AmountInWallet * coinValues[1].PriceEur).Sum(),
+                TradesBuyValueEUR = buys.Where(b => b.Purpose == "Trade").Select(b => b.AmountBought * b.PriceEur).Sum(),
+                TradesSellValueEUR = trades.Select(t => t.Amount * coinValues[t.CoinId].PriceEur).Sum(),
+                CreationsBuyValueEUR = 0,
+                CreationsSellValueEUR = creations.Select(c => c.Amount * coinValues[c.CoinId].PriceEur).Sum()
+            };
+        }
+
+
+
     }
 }
---
+++ CoinView/Models/UserSummaryDO.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoinView.Models {
+    public class UserSummaryDO {
+
+        public int UserID;
+        public decimal InvestsBuyValueEUR { get; set; }
+        public decimal InvestsSellValueEUR { get; set; }
+        public decimal InvestsResultValueEUR { get { return InvestsSellValueEUR - InvestsBuyValueEUR; } }
+        public decimal TradesBuyValueEUR { get; set; }
+        public decimal TradesSellValueEUR { get; set; }
+        public decimal TradesResultValueEUR { get { return TradesSellValueEUR - TradesBuyValueEUR; } }
+        public decimal CreationsBuyValueEUR { get; set; }
+        public decimal CreationsSellValueEUR { get; set; }
+        public decimal CreationsResultValueEUR { get { return CreationsSellValueEUR - CreationsBuyValueEUR; } }
+
+        public decimal SellTotalEUR { get { return InvestsSellValueEUR + TradesSellValueEUR + CreationsSellValueEUR; } }
+        public decimal BuyTotalEUR { get { return InvestsBuyValueEUR + TradesBuyValueEUR + CreationsBuyValueEUR; } }
+        public decimal ResultTotalEUR { get { return SellTotalEUR - BuyTotalEUR; } }
+    }
+}
